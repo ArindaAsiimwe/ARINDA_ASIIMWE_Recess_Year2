26f68e7befb7649df0ea643db0854bcf022cdb01
ArindaAsiimwe <aritah185@gmail.com>
2024-06-07 11:09:46 +0300

    Day01

---
+++ test_Day01.py
@@ -0,0 +1,27 @@
+print('test')
+x=5
+y=30
+print(x+y)
+
+name = input("Enter your name: ")
+fav_color = input("Enter your favorite color: ")
+print(name + " likes " + fav_color)
+
+#this is how single-line comments are written
+"""this is a 
+multiline comment
+in python"""
+
+''' this is also 
+a multi-line
+comment
+'''
+
+my_tuple=("Arinda", "Taras", "Tahia","Becka","Vanessa", 78)
+print(type(my_tuple))
+print(my_tuple[1])
+
+print('The sum is %.1f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
+
+
+
